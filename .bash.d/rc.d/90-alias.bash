0f6af2b46d2ec31add5ab7f8283e662c29edeae2
Kikyou Akino <bellflower@web4u.jp>
2015-09-14 10:24:03 +0900

    fix detection procedure of ls

--- .bash.d/rc.d/90-alias.bash
+++ .bash.d/rc.d/90-alias.bash
@@ -1,13 +1,12 @@
 unalias ls 2>/dev/null
 
+alias ls="ls -G"
 if [ "$(type gnuls 2>/dev/null)" ]; then
     alias ls='gnuls --color=auto'
 elif [ "$(type gls 2>/dev/null)" ]; then
     alias ls='gls --color=auto'
-elif [ "$(ls --color 2>/dev/null)" ]; then
+elif [ "$(/bin/ls --version 2>&1 | grep -s GNU)" ]; then
     alias ls='ls --color=auto'
-elif [ "$(ls -G 2>/dev/null)" ]; then
-    alias ls="ls -G"
 fi
 
 alias ll='ls -alF'
