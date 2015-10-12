6c029f8b86df8ae262aca412e8a1f62bb0b90768
Kikyou Akino <bellflower@web4u.jp>
2015-10-13 02:58:12 +0900

    Avoid warning messages for detecting ls command

--- .bash.d/rc.d/90-alias.bash
+++ .bash.d/rc.d/90-alias.bash
@@ -4,7 +4,7 @@ if [ "$(type gnuls 2>/dev/null)" ]; then
     alias ls='gnuls --color=auto'
 elif [ "$(type gls 2>/dev/null)" ]; then
     alias ls='gls --color=auto'
-elif [ "$(/bin/ls --version | grep -s GNU)" ]; then
+elif [ "$(/bin/ls --version 2>/dev/null| grep -s GNU)" ]; then
     alias ls='ls --color=auto'
 else
     alias ls='ls -G'
